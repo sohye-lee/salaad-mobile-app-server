49d8d04516870f4a8648ce2f2ad753ebaa4e7540
Sohye Lee <sohyelee@gmail.com>
2020-12-10 13:07:21 -0500

    basic route modified

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/', routes);
+app.use('/salaad', routes);
 
 const PORT = process.env.PORT || 5000;
 
